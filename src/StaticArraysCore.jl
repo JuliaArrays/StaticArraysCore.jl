48972b4213df497d510e12cb84a0eabc8ac80c97
Steve Kelly <kd2cca@gmail.com>
2024-04-17 12:47:10 -0400

    Update src/StaticArraysCore.jl
    Co-authored-by: Mateusz Baran <mateuszbaran89@gmail.com>

--- src/StaticArraysCore.jl
+++ src/StaticArraysCore.jl
@@ -286,7 +286,7 @@ For detailed usage and functionality, refer to the documentation of `SizedArray`
 const SizedVector{S,T} = SizedArray{Tuple{S},T,1,1}
 
 """
-    SizedMatrix{S1,S2,T} <: SizedArray{Tuple{S1,S2},T,2}
+    SizedMatrix{S1,S2,T} = SizedArray{Tuple{S1,S2},T,2}
 
 Wraps a two-dimensional `AbstractArray` with static dimensions `S1` by `S2` and element type `T`,
 leveraging the performance optimizations of StaticArrays.jl.
