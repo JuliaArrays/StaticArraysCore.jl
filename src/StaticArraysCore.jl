2cc2c8e96e83f21ebc44e7e243523426e2282eb2
Abhro R <5664668+abhro@users.noreply.github.com>
2024-06-29 15:41:16 -0400

    Use multiplication sign in place of x

--- src/StaticArraysCore.jl
+++ src/StaticArraysCore.jl
@@ -259,7 +259,7 @@ array may be reshaped.
 
 The aliases `SizedVector{N}` and `SizedMatrix{N,M}` are provided as more
 convenient names for one and two dimensional `SizedArray`s. For example, to
-wrap a 2x3 array `a` in a `SizedArray`, use `SizedMatrix{2,3}(a)`.
+wrap a 2×3 array `a` in a `SizedArray`, use `SizedMatrix{2,3}(a)`.
 """
 struct SizedArray{S<:Tuple,T,N,M,TData<:AbstractArray{T,M}} <: StaticArray{S,T,N}
     data::TData
